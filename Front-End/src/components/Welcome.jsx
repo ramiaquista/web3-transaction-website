c8a420385ec87e153ca7a53417c63d148fedd3c5
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-03-17 10:48:50 -0300

    Update welcome component

--- Front-End/src/components/Welcome.jsx
+++ Front-End/src/components/Welcome.jsx
@@ -34,7 +34,7 @@ const Welcome = () => {
           </p>
           <button
             type="button"
-            onCliclk={connectWallet}
+            onClick={connectWallet}
             className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
           >
             <p className="text-white text-base font-semibold">Connect Wallet</p>
